d8dc10419bd7babbfd4d82881701407362ba589e
사이렉스페이 <139845533+CyrexpayPG@users.noreply.github.com>
2023-07-19 05:31:12 +0000

    Update 3 files

--- webhook.php
+++ webhook.php
@@ -1,4 +1,7 @@
 <?php
-echo 'into webhook ';
-
+echo 'webhook (Backend async request)';
+$post_raw = file_get_contents('php://input');
+echo $post_raw;
+// $post_data = json_decode($post_raw);
+// trxId or trackId 중복체크 필수
 ?>
